39f927e60d857eddb423f5fcae86bebd3ca0606e
Lailson Henrique <lailsonh@hotmail.com>
2012-06-24 11:10:18 -0300

    Relatorio Hardening

---
+++ Hardening/relatorio.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Copiando arquivos
+cp /etc/passwd /home/lailson/so2/arquivos/passwd
+cp /etc/passwd.old /home/lailson/so2/arquivos/passwd.old
+
+cp /etc/pam.d/common-auth /home/lailson/so2/arquivos/common-auth
+cp /etc/pam.d/common-auth.old /home/lailson/so2/arquivos/common-auth.old
+cp /etc/pam.d/common-account /home/lailson/so2/arquivos/common-account
+cp /etc/pam.d/common-account.old /home/lailson/so2/arquivos/common-account.old
+
+cp /etc/pam.d/common-password /home/lailson/so2/arquivos/common-password
+cp /etc/pam.d/common-password.old /home/lailson/so2/arquivos/common-password.old
+
+cp /etc/profile /home/lailson/so2/arquivos/profile
+cp /etc/profile.old /home/lailson/so2/arquivos/profile.old
+
+cp /etc/securetty /home/lailson/so2/arquivos/securetty
+cp /etc/securetty.old /home/lailson/so2/arquivos/securetty.old
+
+cp /etc/pam.d/su /home/lailson/so2/arquivos/securetty
+cp /etc/pam.d/su.old /home/lailson/so2/arquivos/securetty.old
+
+cp /etc/security/limits.conf /home/lailson/so2/arquivos/limits.conf
+cp /etc/security/limits.conf.old /home/lailson/so2/arquivos/limits.conf.old
+
+cp /etc/pam.d/login /home/lailson/so2/arquivos/login
+cp /etc/pam.d/login.old /home/lailson/so2/arquivos/login.old
+
